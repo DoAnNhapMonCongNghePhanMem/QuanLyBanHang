e13f069317cdd9823d005aefe1624018db134334
LeCongKhanh1810 <15520355@gm.uit.edu.vn>
2018-05-27 20:32:31 +0700

    DAL Update

---
+++ QuanLyDaiLy_src/DAL_QuanLyDaiLy/DAL_LoaiDaiLy.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL_QuanLyDaiLy
+{
+    public class DAL_LoaiDaiLy
+    {
+        private static SqlConnection conn = DBUtils.GetDBConnection();
+        public static ArrayList GetLoaiDL()
+        {
+            ArrayList arrList = new ArrayList();
+            DataTable dt = new DataTable();
+            dt = ResultQuery.GetTableResult(conn, "SELECT * FROM LoaiDaiLy ");
+            int id;
+            string ten;
+            foreach (DataRow r in dt.Rows)
+            {
+                id = (int)r["IdLoaiDL"];
+                ten = r["TenLoaiDL"].ToString();
+                DTO_LoaiDL loaiDL = new DTO_LoaiDL(id, ten);
+                arrList.Add(loaiDL);
+            }
+
+            return arrList;
+        }
+    }
+}
