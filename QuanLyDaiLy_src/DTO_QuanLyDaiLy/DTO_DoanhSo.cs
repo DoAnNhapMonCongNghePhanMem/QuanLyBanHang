1359a40ed429bdc5cb94f69c73e3deba99bac264
LeCongKhanh1810 <15520355@gm.uit.edu.vn>
2018-05-28 15:24:15 +0700

    DAL update

---
+++ QuanLyDaiLy_src/DTO_QuanLyDaiLy/DTO_DoanhSo.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO_QuanLyDaiLy
+{
+    public class DTO_DoanhSo
+    {
+        private int idDaiLy;
+        private int thang;
+        private float tongDoanhSo;
+        private int soPhieuXuat;
+        private float tiLe;
+
+        public int IdDaiLy
+        {
+            get
+            {
+                return idDaiLy;
+            }
+
+            set
+            {
+                idDaiLy = value;
+            }
+        }
+
+        public int Thang
+        {
+            get
+            {
+                return thang;
+            }
+
+            set
+            {
+                thang = value;
+            }
+        }
+
+        public float TongDoanhSo
+        {
+            get
+            {
+                return tongDoanhSo;
+            }
+
+            set
+            {
+                tongDoanhSo = value;
+            }
+        }
+
+        public int SoPhieuXuat
+        {
+            get
+            {
+                return soPhieuXuat;
+            }
+
+            set
+            {
+                soPhieuXuat = value;
+            }
+        }
+
+        public float TiLe
+        {
+            get
+            {
+                return tiLe;
+            }
+
+            set
+            {
+                tiLe = value;
+            }
+        }
+
+        public DTO_DoanhSo()
+        {
+
+        }
+        public DTO_DoanhSo(int idDaiLy,int thang,float tongDoanhSo,int soPhieuXuat,float tiLe)
+        {
+
+        }
+    }
+}
